21fc4ec91934754d39712d4e02d10b6295a6f4d0
gabidron <drongabi8@gmail.com>
2020-04-03 18:40:18 +0300

    script pt movement

---
+++ New Unity Project/Assets/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+    void Start()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
