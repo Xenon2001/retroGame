e897615974447c347634d1753ca158277d5efd1d
Munteanu Teofil <andreiteofil01@yahoo.com>
2020-04-07 22:54:44 +0300

    Animatii
    Pt a schimba intre movementuri dati check pe casuta din stanga numelui scriptului

--- New Unity Project/Assets/scripts/playerMovement.cs
+++ New Unity Project/Assets/scripts/playerMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 
-    public float speed = 5f;
+    public float speed;
     public Rigidbody2D rb;
+    public Animator animator;
     Vector2 movement;
     void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Horizontal", movement.x);
+
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate() {
