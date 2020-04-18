70c9b2e9e6c5fb2a8d61cc5dabd2a46790cfe581
gabidron <drongabi8@gmail.com>
2020-04-18 20:57:49 +0300

    test

---
+++ New Unity Project/Assets/Scripts/changeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public string sceneChance;
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+                
+        if (collision.CompareTag("Player"))
+        {
+            if(sceneChance == "next")
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex  - 1);
+            }
+            
+        }
+    }
+}
