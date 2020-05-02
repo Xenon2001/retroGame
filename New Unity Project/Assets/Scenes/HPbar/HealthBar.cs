02248bae2528a043d199d5e6d1f553ec7f273c12
Pflorin11 <63008886+Pflorin11@users.noreply.github.com>
2020-05-02 16:00:38 +0300

    hpbar

---
+++ New Unity Project/Assets/Scenes/HPbar/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Text hpAmount;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        hpAmount.text = health + "/100";
+    }
+
+}
