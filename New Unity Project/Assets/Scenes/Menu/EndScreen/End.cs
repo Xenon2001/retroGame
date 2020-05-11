d7fede11b7a7a5135d56422873224412d3542763
Pflorin11 <63008886+Pflorin11@users.noreply.github.com>
2020-05-11 22:30:22 +0300

    EndMenu canvas&scripts

---
+++ New Unity Project/Assets/Scenes/Menu/EndScreen/End.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    public GameObject endMenu;
+    private bool end;
+
+    void Update()
+    {
+        //CONDITIE DE END
+        if (end)
+        {
+            endMenu.SetActive(true);
+
+        }
+    }
+
+}
